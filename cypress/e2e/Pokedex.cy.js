0b9702ceba5f67cc4a6bc97ce6b8d7768cae9aac
DN123@gmail.com <DN.gmail.com>
2024-01-17 16:23:55 +0800

    addednavigationtest

--- cypress/e2e/Pokedex.cy.js
+++ cypress/e2e/Pokedex.cy.js
@@ -5,4 +5,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('can navigate from the main page to the page of a particular Pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('squirtle').click()
+    cy.contains('squirtle')
+    cy.contains('speed')
+  })
+
 })
