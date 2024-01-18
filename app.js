040ac014507758f8fc35e8cde01a37aece0a617a
DN123@gmail.com <DN.gmail.com>
2024-01-18 15:08:02 +0800

    try health check error

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
