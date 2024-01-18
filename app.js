e876e882d33f4e1afb5a57730ca9cab3c87b3e79
DN123@gmail.com <DN.gmail.com>
2024-01-18 14:07:46 +0800

    add deploy hook

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
